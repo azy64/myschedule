68a35ba33ebc429a1e5d971cda3f28fd603886eb
cecethea <ui564839@rwe-ag.com>
2025-12-10 14:03:21 +0100

    update email template

--- src/EventSubscriber/ConfirmRendezVousSubscriber.php
+++ src/EventSubscriber/ConfirmRendezVousSubscriber.php
@@ -2,12 +2,9 @@
 namespace App\EventSubscriber;
 
 use App\Event\ConfirmEvent;
-use phpDocumentor\Reflection\DocBlock\Tags\Template;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
 
  class ConfirmRendezVousSubscriber implements EventSubscriberInterface
 {
@@ -20,20 +17,23 @@ use Symfony\Component\Mime\Email;
 
     public function onConfirmRendezVous(ConfirmEvent $event): void
     {
-        echo"je suis là";
+        //echo"je suis là";
         $visitorList = $event->getVisitorList();
         $patient = $visitorList->getPatient();
         $mailerInterface = $event->getMailerInterface();
         $mail =(new TemplatedEmail)
             ->from(new Address('MS_cWdUfE@test-p7kx4xw9epvg9yjr.mlsender.net',"Tunaweza-Myschedule"))
-            
-            ->to($patient->getEmail())
+            ->to(new Address($patient->getEmail(),$patient->getNom().' '.$patient->getPrenom()))
             ->subject('Confirmation de votre rendez-vous')
-            //->html('<p>Votre rendez-vous a été confirmé.<br/> Send with symfony mailer.</p>')
             ->htmlTemplate("inscription/myNumberEmailTemplate.html.twig")
             ->context([
                "schedule"=>$visitorList,
             ]);
-        $mailerInterface->send($mail);
+            try{
+                $mailerInterface->send($mail);
+            }
+            catch(\Exception $e){
+                echo "Erreur d'envoi de mail: ".$e->getMessage();
+            }
     }
 }
